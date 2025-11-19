be1448f724f2b310fa055b4256b2746f26d765e8
Levhcwm21. <alexags1983@gmail.com>
2025-11-19 16:15:12 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,5 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URL, DB_NAME
 
-_client = AsyncIOMotorClient(MONGO_URL)
-db = _client[DB_NAME]
+class Database:
+    def __init__(self, uri):
+        self.uri = uri
+        self.client = None
+        self.db = None
+
+    async def connect(self):
+        self.client = AsyncIOMotorClient(self.uri)
+        self.db = self.client.tmdbbot
+        self.search_cache = self.db.search_cache
+        self.details_cache = self.db.details_cache
+        self.trending_cache = self.db.trending_cache
